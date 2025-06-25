efb08b77ae8944628bab028c3a8fab70916b27d2
UnChosenUndead <go.prog.freelance@hotmail.com>
2025-06-26 00:57:52 +0300

    add .env for test ,add tests, mini fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,12 @@
+use dotenv::dotenv;
 use reqwest::Error;
 use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Default)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct Conf {
-    pub app: AppConf,
-    pub db: DBConf,
-}
-
-#[derive(Serialize, Deserialize, Default)]
-pub struct AppConf {
-    pub port: String,
-    pub host: String,
-}
-
-#[derive(Serialize, Deserialize, Default)]
-pub struct DBConf {
     pub pg_db_connection_port: String,
     pub pg_db_host: String,
+    pub port: String,
+    pub host: String,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -24,18 +14,25 @@ struct ReqStruct {
     app_name: String,
 }
 
-impl Conf {
-    pub async fn get_config(app_name: String) -> Result<Conf, reqwest::Error> {
-        if cfg!(debug_assertions) {
-            match envy::prefixed(app_name).from_env::<Conf>() {
-                Ok(config) => Ok(config),
-                Err(error) => panic!("cant parse config {}", error),
-            }
-        } else {
-            match fetch_config_from_server(app_name).await {
-                Ok(conf) => Ok(conf),
-                Err(error) => panic!("fall at fetch conf from server {}", error),
-            }
+pub fn testetset() {
+    dotenv().ok();
+
+    let x: Conf = envy::from_env().unwrap();
+    println!("{:?}", x)
+}
+
+pub async fn get_config(app_name: String) -> Result<Conf, reqwest::Error> {
+    let formated_app_name = format!("{}_", app_name.to_uppercase());
+    if cfg!(debug_assertions) {
+        dotenv().ok();
+        match envy::prefixed(formated_app_name).from_env::<Conf>() {
+            Ok(config) => Ok(config),
+            Err(error) => panic!("cant parse config {}", error),
+        }
+    } else {
+        match fetch_config_from_server(app_name).await {
+            Ok(conf) => Ok(conf),
+            Err(error) => panic!("fall at fetch conf from server {}", error),
         }
     }
 }
@@ -43,19 +40,63 @@ impl Conf {
 async fn fetch_config_from_server(app_name: String) -> Result<Conf, Error> {
     let client = reqwest::Client::new();
     let response: Conf = client
-        .post("http://localhost:20200/conf")
+        .post("http://127.0.0.1:20200/conf")
+        .header("content-type", "application/json")
         .json(&ReqStruct { app_name })
         .send()
-        .await?
+        .await
+        .unwrap()
         .json()
-        .await?;
-
+        .await
+        .unwrap();
+    println!("{:?}", response);
     Ok(response)
 }
 
 #[cfg(test)]
 mod tests {
 
-    #[test]
-    fn it_works() {}
+    use crate::{Conf, ReqStruct, fetch_config_from_server, get_config};
+    use mockito::Matcher::Json;
+    use serde_json::json;
+
+    fn init_test_conf() -> Conf {
+        Conf {
+            port: "test".to_string(),
+            host: "test".to_string(),
+            pg_db_connection_port: "test".to_string(),
+            pg_db_host: "test".to_string(),
+        }
+    }
+
+    #[tokio::test]
+    async fn fetch_config_from_server_test() {
+        let response_body = serde_json::to_string(&init_test_conf()).unwrap();
+        let opts = mockito::ServerOpts {
+            host: "127.0.0.1",
+            port: 20200,
+            ..Default::default()
+        };
+        println!("{}", response_body);
+        let mut server = mockito::Server::new_with_opts(opts);
+
+        let _mock = server
+            .mock("POST", "/conf")
+            .match_body(Json(json!(ReqStruct {
+                app_name: "test".to_string()
+            })))
+            .match_header("content-type", "application/json")
+            .with_status(200)
+            .with_body(response_body)
+            .create();
+        let x = fetch_config_from_server("test".to_string()).await.unwrap();
+        _mock.assert();
+        assert!(x == init_test_conf(), "true")
+    }
+
+    #[tokio::test]
+    async fn get_config_test() {
+        let x = get_config("test".to_string()).await.unwrap();
+        assert!(x == init_test_conf(), "true")
+    }
 }
